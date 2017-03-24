694be1a697224436fbe05843e106e53e1cc518ee
alexanderkatz <katzkode@gmail.com>
2017-03-24 12:11:41 -0500

    Adding toJSON

--- js/binarysearchtree.js
+++ js/binarysearchtree.js
@@ -1,22 +1,3 @@
-/* 
-A Binary Search Tree is a binary tree with the following properties.
-
-"Each node of a Binary Search Tree (BST) stores a piece of data, called the key. Each node has below it a left subtree and a right subtree. The topmost node is called the root and a node with no subtrees is called a leaf. 
-
-The most important property of a BST is that 
-for a node with key k, every key in the left subtree is less than k and every key in the right subtree is greater than k."
-
-http://research.cs.queensu.ca/~jstewart/applets/bst/bst-property.html
-*/
-
-/* Node
-   The node class defines a constructor that
-   sets the node's value to the supplied argument
-   and sets the left and right child to null.
-   
-   This class is used by the BinarySearchTree class to
-   structure its data, it is not used by the main program
-*/
 class Node{
     constructor(value){
         this.value = value;
@@ -69,6 +50,31 @@ class BinarySearchTree{
             }
         }
     }
+    /*To JSON, preorder traversal*/
+    toJSON(){
+        // var data = {};
+        // var current = this.root;
+        // var i=0;
+        
+        var stack = [];
+        stack.push(this.root);
+        
+        while(stack.length>0){
+            var current = stack.pop(); 
+            console.log(current);
+
+            if(current.right != null){
+                stack.push(current.right);
+            }
+            if(current.left != null){
+                stack.push(current.left);
+            }
+
+        }
+
+
+        // console.log("DATA",data);
+    }
 /*End of Class*/    
 }
 
@@ -76,12 +82,221 @@ class BinarySearchTree{
 // Main Program
 function main(){
     var tree = new BinarySearchTree(10);
-    var numbers = [5,2,66,34,52,12,6,8,15];
+    var numbers = [7,5,9,20];
     
     for (var i =0; i< numbers.length; i++){
         tree.insert(numbers[i]);
     }
-    console.log(tree);    
+    // console.log(tree);  
+    tree.toJSON();  
+    
+    // https://bl.ocks.org/d3noob/43a860bc0024792f8803bba8ca0d5ecd
+// DATA
+
+var treeData = {
+    "value": "10",
+    "children": [{
+            "value": "7",
+            "children": [{
+                    "value": "5"
+                },
+                {
+                    "value": "9"
+                }
+            ]
+        },
+        {
+            "value": "20"
+        }
+    ]
+};
+console.log(treeData);
+
+// Set dimensions and margins for diagram
+var margin = {top: 80, right: 90, bottom: 20, left: 90},
+    width = 960 - margin.right - margin.left,
+    height = 500 - margin.top - margin.bottom;
+    
+    
+// append the svg object to the body of the page
+// appends a 'group' element to 'svg'
+// moves the 'group' element to the top left margin   
+var svg = d3.select("body").append("svg")
+            .attr("width", width + margin.right + margin.left)
+            .attr("height", height + margin.top + margin.bottom)
+           .append("g")
+            .attr("transform", "translate("
+                + margin.left + "," + margin.top + ")");
+                
+var i = 0,      
+    duration = 750,
+    root;
+
+// Declares a tree layout and assigns the size
+var treemap = d3.tree().size([height, width]);
+
+// Assigns parent, children, height, depth
+root = d3.hierarchy(treeData, function(d){ return d.children; });
+root.x0 =  height/ 2;
+root.y0 = 0;
+
+console.log(root);
+
+// Collapse after the second level
+root.children.forEach(collapse);
+
+update(root);
+
+// Collapse the node and all it's children
+function collapse(d){
+    if(d.children){
+        d._children = d.children
+        d._children.forEach(collapse)
+        d.children = null;
+    }
+}
+
+// Update
+function update(source){
+    // Assigns the x and y position for the nodes
+    var treeData = treemap(root);
+    
+    // Compute the new tree layout.
+    var nodes = treeData.descendants(),
+        links = treeData.descendants().slice(1);
+        
+    // Normalize for fixed-depth
+    nodes.forEach(function(d){ d.y = d.depth*100 });    
+    
+    // **************** Nodes Section ****************
+    
+    // Update the nodes...
+    var node = svg.selectAll('g.node')
+         .data(nodes, function(d) {return d.id || (d.id = ++i); });
+         
+    // Enter any new nodes at the parent's previous position.
+    var nodeEnter = node.enter().append('g')
+                     .attr('class', 'node')
+                     .attr("transform", function(d){
+                         return "translate(" + source.x0 + "," + source.y0 + ")";
+                     })
+                     .on('click', click);
+                     
+    // Add Circle for the nodes
+    nodeEnter.append('circle')
+        .attr('class', 'node')
+        .attr('r', 1e-6)
+        .style("fill", function(d){
+            return d._children ? "lightsteelblue" : "#fff";
+        });
+    
+    // Add labels for the nodes
+    nodeEnter.append('text')
+        .attr("dy", ".35em")
+        .attr("x", function(d){
+            return d.children || d._children ? -13 : 13;
+        })
+        .attr("text-anchor", function(d){
+            return d.children || d._children ? "end" : "start";
+        })
+        .text(function(d){ return d.data.value; });
+    
+    // Update
+    var nodeUpdate = nodeEnter.merge(node);
+    
+    // Transition to the proper position for the nodes
+    nodeUpdate.transition()
+        .duration(duration)
+        .attr("transform", function(d) {
+            return "translate(" + d.x + "," + d.y + ")";
+        });
+    
+    // Update the node attributes and style
+    nodeUpdate.select('circle.node')
+        .attr('r', 10)
+        .style("fill", function(d){
+            return d._children ? "lightsteelblue" : "#fff";
+        })
+        .attr('cursor', 'pointer');
+        
+    // Remove any exiting nodes
+    nodeExit = node.exit().transition()
+        .duration(duration)
+        .attr("transform", function(d){
+            return "translate(" + source.x +","+ source.y +")";
+        })
+        .remove();
+        
+    // On exit reduce the node circles size to 0
+    nodeExit.select('circle')
+        .attr('r', 1e-6);
+    
+    // On exit reduce the opacity of text lables  
+    nodeExit.select('text')
+        .style('fill-opacity', 1e-6)
+        
+    // **************** Links Section ****************
+    
+    // Update the links...
+    var link = svg.selectAll('path.link')
+        .data(links, function(d){ return d.id; });
+        
+    // Enter any new links at the parent's previous position
+    var linkEnter = link.enter().insert('path', "g")
+        .attr("class", "link")
+        .attr('d', function(d){
+            var o = {x: source.x0, y: source.y0};
+            return diagonal(o,o);
+        });
+    
+    // Update
+    var linkUpdate = linkEnter.merge(link);
+    
+    // Transition back to the parent element position
+    linkUpdate.transition()
+        .duration(duration)
+        .attr('d', function(d){ return diagonal(d, d.parent) });
+    
+    // Remove any existing links
+        var linkExit = link.exit().transition()
+            .duration(duration)
+            .attr('d', function(d){
+                var o = {x: source.x, y: source.y};
+            })
+            .remove();
+    
+    // Store the old positions for transition.
+    nodes.forEach(function(d){
+        d.x0 = d.x;
+        d.y0 = d.y;
+    });
+    
+    // Create a curved (diagonal) path from parent to the child nodes
+    function diagonal(s,d){
+        path = `M ${s.x} ${s.y}
+        C ${(s.x + d.x) / 2} ${s.y},
+          ${(s.x + d.x) / 2} ${d.y},
+          ${d.x} ${d.y}`
+
+        return path;
+    }
+
+    // Toggle children on click
+    function click(d){
+        if (d.children){
+            d._children = d.children;
+            d.children = null;
+        }
+        else{
+            d.children = d._children;
+            d._children = null;
+        }
+        update(d);
+    }
+}
+
+// console.log(nodes);
+// console.log(links);
 }
 
 // Call main
